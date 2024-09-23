82f1170c1839009b4874a73b21220b9afbbafd72
samanvay13 <tripathisamanvay13@gmail.com>
2024-09-23 13:03:15 +0530

    Featured post

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,9 +1,29 @@
 import React from "react";
 import styles from './featured.module.css';
+import Image from "next/image";
 
 const Featured = () => {
   return (
-    <div className={styles.container}>Featured</div>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <b>Stay ahead with Bulletin!</b> Your source for the latest in web development.
+      </h1>
+      <div className={styles.post}>
+        <div className={styles.imgContainer}>
+          <Image src='/ai.png' alt='' fill />
+        </div>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</h1>
+          <p className={styles.postDescription}>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Est architecto unde ipsum quae earum a necessitatibus
+            quisquam molestias, labore sequi aut optio eum praesentium
+            ullam obcaecati cumque facere voluptatem provident!
+          </p>
+          <button className={styles.button}>Read More</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
